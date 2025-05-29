21c5a7374721a1828599fd7e1b6dd4e1fbe3a9e1
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-29 14:20:12 -0700

    initial functions setup

---
+++ functions.py
@@ -0,0 +1,26 @@
+def sum(data):
+    return
+
+def length(data):
+    return
+ 
+def sort(data):
+    return
+
+def average(data):
+    return
+
+def min(data):
+    return
+
+def max(data):
+    return
+
+def median(data):
+    return
+
+def quartile(data):
+    return
+
+def standard_deviation(data):
+    return
