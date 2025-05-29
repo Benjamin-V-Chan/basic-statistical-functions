8dcbbafbd8c89da09bc976329f7ba0ae375444c8
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-29 14:21:27 -0700

    min and max functions complete

--- functions.py
+++ functions.py
@@ -17,10 +17,18 @@ def average(data):
     return sum(data) / length(data) 
 
 def min(data):
-    return
+    lowest = data[0]
+    for data_point in data[1:]:
+        if data_point < lowest:
+            lowest = data_point
+    return lowest
 
 def max(data):
-    return
+    highest = data[0]
+    for data_point in data[1:]:
+        if data_point > highest:
+            highest = data_point
+    return highest
 
 def median(data):
     return
